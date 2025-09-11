9bb75c8e6a56a855e3383486d8e14b633d2d8ef2
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-09-11 18:09:46 +0200

    feat: start up boot logs

--- cmd/main.go
+++ cmd/main.go
@@ -3,40 +3,78 @@ package main
 import (
 	"html/template"
 	"io"
+	"log"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct {
-    templates *template.Template
+	templates *template.Template
 }
 
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-    return t.templates.ExecuteTemplate(w, name, data)
+func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func newTemplate() *Templates {
-    return &Templates{
-        templates: template.Must(template.ParseGlob("views/*.html")),
-    }
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 }
 
 type Count struct {
-    Count int
+	Count int
+}
+
+type Terminal struct {
+	StdOut string
+	StdErr string
+	StdIn  string
+}
+
+func readBootText() string {
+	file, err := os.Open("static/boot.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	b, err := io.ReadAll(file)
+	return string(b)
 }
 
 func main() {
-    e := echo.New()
-    e.Use(middleware.Logger())
+	e := echo.New()
+	e.Use(middleware.Logger())
 
-    e.Renderer = newTemplate()
+	e.Renderer = newTemplate()
 
-    count := Count{ Count: 0 }
-    e.GET("/", func(c echo.Context) error {
-        count.Count++
-        return c.Render(200, "index", count)
-    })
+	e.Static("/static", "static")
 
-    e.Logger.Fatal(e.Start(":4000"))
+	terminal := Terminal{StdOut: "", StdErr: "", StdIn: ""}
+	e.GET("/", func(c echo.Context) error {
+		terminal.StdOut = readBootText()
+		return c.Render(200, "index", terminal)
+	})
+
+	e.POST("/input", func(c echo.Context) error {
+		terminal.StdIn = c.FormValue("stdin")
+		return c.Render(200, "terminal", terminal)
+	})
+
+	e.POST("/submit", func(c echo.Context) error {
+		log.Println("Submit")
+		terminal.StdOut += "\n" + terminal.StdIn
+		terminal.StdIn = ""
+		return c.Render(200, "terminal", terminal)
+	})
+
+	e.Logger.Fatal(e.Start(":4000"))
 }
